5cc76cd37771bf5926d04e7961d81229db2fda8e
muriloPereira006 <muriloedu006@gmail.com>
2023-11-13 20:17:00 -0300

    add: authorization

--- src/helpers/api.jsx
+++ src/helpers/api.jsx
@@ -1,12 +1,15 @@
 import axios from 'axios';
 
+const instance = axios.create({
+    baseURL: 'https://api.api-futebol.com.br/v1/',
+    timeout: 1000,
+    headers: { 'Authorization': 'Bearer test_abfb828c31c0f7466840aa36c3d41e' }
+    // headers: { 'Authorization': 'Bearer live_b94f2b2aab20044d2601c4036b797a' }
+});
+
 export async function getBrasileiraoATable() {
     try {
-        const response = await axios.get('https://api.api-futebol.com.br/v1/campeonatos', {
-            headers: {
-                'Authorization': 'Bearer test_abfb828c31c0f7466840aa36c3d41e'
-            }
-        });
+        const response = await instance.get('campeonatos')
         return response;
     } catch (error) {
         console.error(error);
