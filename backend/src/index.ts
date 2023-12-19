2e882694a5e928904f9c79342f6386614bbe1167
muriloPereira006 <muriloedu006@gmail.com>
2023-12-18 23:11:48 -0300

    add: axios and test setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,22 @@
 import { Elysia } from "elysia";
+import axios from 'axios';
+import * as dotenv from 'dotenv';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const PORT =  process.env.PORT || '';
+const baseUrl = process.env.URL_BASE_API_FUTEBOL || '';
+const token = process.env.LIVE_API_TOKEN || '';
+
+const instance = axios.create({
+  baseURL: baseUrl,
+  timeout: 1000,
+  headers: {'Authorization': token}
+})
+
+const app = new Elysia().get("/", () => "Hello Elysia").listen(PORT);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
+
+const response = await instance.get(`campeonatos`);
+console.log(response.data);
