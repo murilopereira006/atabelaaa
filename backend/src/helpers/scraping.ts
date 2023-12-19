cf27e30f1c17882870134a3d25e8a0391dc37e8a
muriloPereira006 <muriloedu006@gmail.com>
2023-12-19 03:14:19 -0300

    add: scarping in array

--- backend/src/helpers/scraping.ts
+++ backend/src/helpers/scraping.ts
@@ -2,23 +2,27 @@ import axios from 'axios';
 import * as cheerio from 'cheerio';
 import * as dotenv from 'dotenv';
 
-const url = process.env.URL_REBAIXAMENTO_SERIE_A || ''
+dotenv.config();
+
+const url = process.env.URL_REBAIXAMENTO_SERIE_A || '';
 
 export default async function fetchData() {
-    try {
-        const response = await axios.get(url);
-        const $ = cheerio.load(response.data);
-        const tableRows = $('tr');
-    
-        // Criar um array para armazenar os dados extraídos
-        const rowData: string[] = [];
-        tableRows.each((index, element) => {
-          rowData.push($.html(element));
-        });
+  try {
+    const response = await axios.get(url);
+    const $ = cheerio.load(response.data);
+    const tableCells = $('td');
 
-        return rowData;
-    } catch (error) {
-        console.error('Erro na solicitação:', error);
-        return [];
-    }
+    const cellContent: string[] = [];
+    tableCells.each((index, element) => {
+      const content = $(element).text().trim();
+      if (content) {
+        cellContent.push(content);
+      }
+    });
+
+    return cellContent;
+  } catch (error) {
+    console.error('Erro na solicitação:', error);
+    return [];
+  }
 }
